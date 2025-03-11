13a187991c21036854ca866cbd577ad66f9daf61
chaichor <christianpro313@gmail.com>
2025-03-11 11:08:46 -0600

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import BlurBackground from './components/BlurBackground'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <BlurBackground/>
+    </div>
+  )
+}
+
+export default App
